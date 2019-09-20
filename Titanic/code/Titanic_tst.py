6b668a935757be7ec913a4acd3b97a5cb71b9345
joburgjazzer <33599516+joburgjazzer@users.noreply.github.com>
2019-09-20 08:00:19 +0200

    Titanic data and start code

---
+++ Titanic/code/Titanic_tst.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('Hello')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as sk
+
+pd.read_csv()
